f297a9854ed3892c3f80acbebbb620810423ae9b
Vladimir Muravev <47062204+wladimirms@users.noreply.github.com>
2024-07-21 20:40:38 +0300

    add files

--- copy_repos.sh
+++ copy_repos.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-sudo mkdir /opt/ython-backend-project
-sudo curl -L "https://github.com/wladimirms/shvirtd-example-python/archive/refs/heads/main.zip" -o /opt/python-backend-project/repo.zip
+sudo apt update
 sudo apt install -y unzip
+sudo mkdir /opt/python-backend-project
+sudo curl -L "https://github.com/wladimirms/shvirtd-example-python/archive/refs/heads/main.zip" -o /opt/python-backend-project/repo.zip
 sudo unzip -j /opt/python-backend-project/repo.zip -d /opt/python-backend-project
 sudo rm /opt/python-backend-project/repo.zip
-
 sudo docker compose up -d
